74e054358c54f9ab9e71999ffaa9d111c6b5f480
Maribel <msevilla318@gmail.com>
2017-03-03 14:23:30 -0500

    primer cambio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+
+  var nombre = document.getElementById('name').value;
+  var apellido = document.getElementById('lastname').value;
+  var email = document.getElementById('input-email').value;
+  var password = document.getElementById('input-password').value;
+  var seleccion = document.getElementById('opciones').selectedIndex;
+
+if(nombre == null || nombre.length == 0 || /^[a-zA-Z]*$/) {
+alert("Necesitas ingresar tu nombre");
+return false;
+}
+else if() {
+  alert();
+  return false;
+}
+else if() {
+  alert();
+  return false;
+}
+else if() {
+  alert();
+  return false;
+}
+return true;
 }
