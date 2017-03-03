8957410adfce47e83ec39fcdf3caaa5106bb6bb8
Maribel <msevilla318@gmail.com>
2017-03-03 16:53:50 -0500

    seguimos cambiando

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ function validateForm(){
   var password = document.getElementById('input-password').value;
   var seleccion = document.getElementById('opciones').selectedIndex;
 
-if(nombre == null || nombre.length == 0 || !/^[a-zA-Z]*$/.test(nombre)) {
+if(nombre == null || nombre.length == 0 || !(/^[a-zA-Z]*$/.test(nombre))) {
 alert("Necesitas ingresar tu nombre");
 return false;
 }
-else if(apellido == null || apellido.length == 0 || !/^[a-zA-Z]*$/.test(apellido)) {
+else if(apellido == null || apellido.length == 0 || !(/^[a-zA-Z]*$/.test(nombre))) {
   alert("Necesitas ingresar tu apellido");
   return false;
 }
